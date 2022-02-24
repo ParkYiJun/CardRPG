7229ba592b218f6caf24e258f3937022bb6b20d0
ParkYiJun <ydlwnsj25@gmail.com>
2022-02-24 16:45:29 +0900

    Base Project

---
+++ Source/CardRPG/CardRPG.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CardRPG.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CardRPG, "CardRPG" );
