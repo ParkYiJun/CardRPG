41ca4362ef719a4b78e25da515899e566d8aece4
ParkYiJun <ydlwnsj25@gmail.com>
2022-03-11 15:09:41 +0900

    HP Bar Setting replaced with Dummy Widget

--- Source/CardRPG/MyUserWidget.cpp
+++ Source/CardRPG/MyUserWidget.cpp
@@ -3,20 +3,40 @@
 
 #include "MyUserWidget.h"
 #include "StatComponent.h"
+#include "Components/CanvasPanelSlot.h"
 #include "Components/ProgressBar.h"
+
+void UMyUserWidget::NativeConstruct() {
+	GaugeSlot = Cast<UCanvasPanelSlot>(Gauge->Slot);
+	if (GaugeSlot) {
+		gaugeWidth = GaugeSlot->GetSize().X;
+		UE_LOG(LogTemp, Warning, TEXT("Width: %f"), gaugeWidth);
+	}
+	
+	UpdateHP();
+}
 
 void UMyUserWidget::BindHp(class UStatComponent* StatComp)
 {
 	CurrentStatComp = StatComp;
-	StatComp->OnHpChanged.AddUObject(this, &UMyUserWidget::UpdateHP);
+	//StatComp->OnHpChanged.AddUObject(this, &UMyUserWidget::UpdateHP);
 }
 
 void UMyUserWidget::UpdateHP()
-{
+{UpdateGauge(0.8f);
 	if (CurrentStatComp.IsValid())
 	{
-		//UE_LOG(LogTemp, Warning, TEXT("UPDATEHP"));
-		PB_HpBar->SetPercent(CurrentStatComp->GetHpRatio());
+		UE_LOG(LogTemp, Warning, TEXT("UPDATEHP"));
+		
+		//PB_HpBar->SetPercent(CurrentStatComp->GetHpRatio());
 		UE_LOG(LogTemp, Warning, TEXT("RATIO: %f"), CurrentStatComp->GetHpRatio());
 	}
+}
+
+void UMyUserWidget::UpdateGauge(float percent) {
+	float finwidth = gaugeWidth * percent;
+
+	FVector2D resizing = FVector2D( finwidth, GaugeSlot->GetSize().Y);
+
+	GaugeSlot->SetSize(resizing);
 }
