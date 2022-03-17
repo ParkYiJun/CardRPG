c8c73b6ab5db53bcdf86413cac07199e3a08bec5
DESKTOP-R0592AU\정우 <luxe04092@naver.com>
2022-03-17 23:45:24 +0900

    11

--- Source/CardRPG/IsPlayerInMeleeRange.cpp
+++ Source/CardRPG/IsPlayerInMeleeRange.cpp
@@ -2,4 +2,31 @@
 
 
 #include "IsPlayerInMeleeRange.h"
+#include "Spidering_AIController.h"
+#include "Spidering.h"
+#include "Runtime/Engine/Classes/Kismet/GameplayStatics.h"
+#include "GameFramework/Character.h"
+#include "Runtime/Engine/Classes/Engine/World.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "blackboard_keys.h"
 
+UIsPlayerInMeleeRange::UIsPlayerInMeleeRange()
+{
+	bNotifyBecomeRelevant = true;
+	NodeName = TEXT("Is Player in Melee Range");
+}
+
+void UIsPlayerInMeleeRange::OnBecomeRelevant(UBehaviorTreeComponent& owner_comp, uint8* node_memory)
+{
+	Super::OnBecomeRelevant(owner_comp, node_memory);
+
+	//get NPC
+	ASpidering_AIController* const cont = Cast<ASpidering_AIController>(owner_comp.GetAIOwner());
+	ASpidering* const spidering = Cast<ASpidering>(cont->GetPawn());
+
+	//get player character
+	ACharacter* const player = UGameplayStatics::GetPlayerCharacter(GetWorld(), 0);
+
+	//write true or false depending on whether the player is within melee_range
+	cont->get_blackboard()->SetValueAsBool(bb_keys::player_is_in_melee_range, spidering->GetDistanceTo(player) <= melee_range);
+}
--- Source/CardRPG/IsPlayerInMeleeRange.h
+++ Source/CardRPG/IsPlayerInMeleeRange.h
@@ -13,5 +13,11 @@ UCLASS()
 class CARDRPG_API UIsPlayerInMeleeRange : public UBTService_BlackboardBase
 {
 	GENERATED_BODY()
+public:
+	UIsPlayerInMeleeRange();
+	void OnBecomeRelevant(UBehaviorTreeComponent& owner_comp, uint8* node_memory) override;
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = AI, meta = (AllowPrivateAccess = "true"))
+	float melee_range = 25.0f;
 	
 };
