99b678abbf69365c6728bd2938156d09dcef8c16
ParkYiJun <ydlwnsj25@gmail.com>
2022-03-24 11:02:53 +0900

    Error Find

---
+++ Source/CardRPG/LevelWidget.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LevelWidget.h"
+#include "Kismet/GamePlayStatics.h"
+#include "StatComponent.h"
+#include "MainCharacter.h"
+
+void ULevelWidget::NativeConstruct() {
+	Super::NativeConstruct();
+
+	class AMainCharacter* player = Cast<AMainCharacter>(UGameplayStatics::GetPlayerCharacter(GetWorld(), 0));
+	if (player) {
+		BindLevel(player->Stats);
+	}
+}
+
+void ULevelWidget::BindLevel(UStatComponent* PStats)
+{
+	CurrentStatComp = PStats;
+	CurrentStatComp->OnXpChanged.AddUObject(this, &ULevelWidget::UpdateLevel);
+}
+
+void ULevelWidget::UpdateLevel() {
+	if (CurrentStatComp.IsValid()) {
+		UE_LOG(LogTemp, Warning, TEXT("Update Level: %d"), CurrentStatComp->GetLevel());
+	}
+}
---
+++ Source/CardRPG/LevelWidget.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "LevelWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CARDRPG_API ULevelWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+	virtual void NativeConstruct() override;
+
+public:
+
+	void BindLevel(UStatComponent* PStats);
+	
+private:
+	TWeakObjectPtr<class UStatComponent> CurrentStatComp;
+	UPROPERTY(meta = (BindWidget))
+		class UTextBlock* LevelText;
+	void UpdateLevel();
+};
