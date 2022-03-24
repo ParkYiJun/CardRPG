a6368b4761ed3f2aef60afa3001b64382b6b6521
ParkYiJun <ydlwnsj25@gmail.com>
2022-03-24 14:55:17 +0900

    Character Level Syncronize

--- Source/CardRPG/LevelWidget.cpp
+++ Source/CardRPG/LevelWidget.cpp
@@ -3,26 +3,33 @@
 
 #include "LevelWidget.h"
 #include "Kismet/GamePlayStatics.h"
+#include "Components/TextBlock.h"
 #include "StatComponent.h"
 #include "MainCharacter.h"
 
+#define LOCTEXT_NAMESPACE "LevelUserWidget"
 void ULevelWidget::NativeConstruct() {
 	Super::NativeConstruct();
 
 	class AMainCharacter* player = Cast<AMainCharacter>(UGameplayStatics::GetPlayerCharacter(GetWorld(), 0));
 	if (player) {
-//		BindLevel(player->Stats);
+		BindLevel(player->Stats);
+		UpdateLevel();
 	}
 }
 
-//void ULevelWidget::BindLevel(UStatComponent* PStats)
-//{
-//	CurrentStatComp = PStats;
-//	CurrentStatComp->OnXpChanged.AddUObject(this, &ULevelWidget::UpdateLevel);
-//}
+void ULevelWidget::BindLevel(class UStatComponent* PStats)
+{
+	CurrentStatComp = PStats;
+	CurrentStatComp->OnXpChanged.AddUObject(this, &ULevelWidget::UpdateLevel);
+}
 
 void ULevelWidget::UpdateLevel() {
 	if (CurrentStatComp.IsValid()) {
 		UE_LOG(LogTemp, Warning, TEXT("Update Level: %d"), CurrentStatComp->GetLevel());
+		FString lvtext = FString::Printf(TEXT("Lv. %02d"), CurrentStatComp->GetLevel());
+		LevelText->SetText(FText::FromString(lvtext));
 	}
 }
+
+#undef LOCTEXT_NAMESPACE
