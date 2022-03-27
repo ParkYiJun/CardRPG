bef142639c99c0e5e70d9a59096b24b5530d964e
ParkYiJun <ydlwnsj25@gmail.com>
2022-03-27 10:40:35 +0900

    monster hp Bar Resize

--- Source/CardRPG/Spidering.cpp
+++ Source/CardRPG/Spidering.cpp
@@ -10,7 +10,6 @@
 #include "Components/WidgetComponent.h"
 #include "UObject/ConstructorHelpers.h"
 #include "Blueprint/UserWidget.h"
-#include "healthBar.h"
 #include "Runtime/Engine/Classes/Components/BoxComponent.h"
 #include "MainCharacter.h"
 #include "StatComponent.h"
@@ -34,12 +33,13 @@ ASpidering::ASpidering() :
 	if (widget_component)
 	{
 		widget_component->SetupAttachment(RootComponent);
-		widget_component->SetWidgetSpace(EWidgetSpace::World);
-		widget_component->SetRelativeLocation(FVector(0.0f, 0.0f, 85.0f));
+		widget_component->SetWidgetSpace(EWidgetSpace::Screen);
+		widget_component->SetRelativeLocation(FVector(0.0f, 0.0f, 100.0f));
 		static ConstructorHelpers::FClassFinder<UUserWidget> widget_class(TEXT("/Game/UI/HPXP/WCBP_HP"));
 		if (widget_class.Succeeded())
 		{
 			widget_component->SetWidgetClass(widget_class.Class);
+			widget_component->SetDrawSize(FVector2D(150.0f, 50.0f));
 		}
 	}
 }
