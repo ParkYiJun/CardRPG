0355f72f2a8e08df43483a3f169cc54ce0d83c47
DESKTOP-3BG11OQ\user <millionku@naver.com>
2022-03-29 10:04:43 +0900

    no message

--- Source/CardRPG/HardAttackSkill.cpp
+++ Source/CardRPG/HardAttackSkill.cpp
@@ -71,7 +71,7 @@ void AHardAttackSkill::Tick(float DeltaTime)
 
 void AHardAttackSkill::OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	if (OtherActor && (OtherActor != this) && OtherComp)
+	if (OtherActor && (OtherActor != this) && OtherComp && OtherActor!=MainCharacter)
 	{
 		GetOtherActor = OtherActor;
 		float WaitTime = 0.3f;
