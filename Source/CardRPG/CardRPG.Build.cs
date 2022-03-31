e4e7ecd5923fd67dea66a55a35ab9e258bcf357b
DESKTOP-3BG11OQ\user <millionku@naver.com>
2022-04-01 01:03:40 +0900

    no message

--- Source/CardRPG/CardRPG.Build.cs
+++ Source/CardRPG/CardRPG.Build.cs
@@ -7,19 +7,19 @@ public class CardRPG : ModuleRules
 	public CardRPG(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
-
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" ,"UMG","HeadMountedDisplay","AIModule","GamePlayTasks", "NavigationSystem", "Slate", "SlateCore" });
 
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG", "HeadMountedDisplay", "AIModule", "GamePlayTasks", "NavigationSystem" });
 
-		// Uncomment if you are using Slate UI
-		PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-	}
+        // Uncomment if you are using online features
+        // PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+
+        if (Target.Type != TargetType.Editor)
+        {
+            throw new BuildException("Unable to instantiate UnrealEd module for non-editor targets.");
+        }
+    }
 }
